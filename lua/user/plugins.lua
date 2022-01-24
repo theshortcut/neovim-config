ba20e61896821e15e367dc270abea92415e84c45
Clayton Ferris <cferris@gmail.com>
2022-01-24 09:54:14 -0700

    colorscheme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -40,10 +40,13 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
+  -- Core plugins
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
+
+  -- Colorschemes
+  use "lunarvim/colorschemes"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
