93c30b6eb42126d282bb82205078798e51a7c5b1
Clayton Ferris <cferris@gmail.com>
2022-09-20 12:12:38 -0700

    simplify and update things

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,36 +1,36 @@
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
-  return
+	return
 end
 
 local rt = require("rust-tools")
 
 rt.setup({
-  tools = {
-    inlay_hints = {
-      auto = true,
-      only_current_line = false,
-      show_parameter_hints = true,
-      parameter_hints_prefix = "<- ",
-      other_hints_prefix = "=> ",
-      max_len_align = false,
-      max_len_align_padding = 1,
-      right_align = false,
-      right_align_padding = 7,
-      highlight = "Comment",
-    },
-  },
+	tools = {
+		inlay_hints = {
+			auto = true,
+			only_current_line = false,
+			show_parameter_hints = true,
+			parameter_hints_prefix = "<- ",
+			other_hints_prefix = "=> ",
+			max_len_align = false,
+			max_len_align_padding = 1,
+			right_align = false,
+			right_align_padding = 7,
+			highlight = "Comment",
+		},
+	},
 
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<Leader>h", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
+	server = {
+		on_attach = function(_, bufnr)
+			-- Hover actions
+			vim.keymap.set("n", "<Leader>h", rt.hover_actions.hover_actions, { buffer = bufnr })
+			-- Code action groups
+			vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+		end,
+	},
 })
 
-require "user.lsp.configs"
+require("user.lsp.lsp-installer")
 require("user.lsp.handlers").setup()
-require "user.lsp.null-ls"
+require("user.lsp.null-ls")
--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,22 +1,24 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
--- local diagnostics = null_ls.builtins.diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-  debug = false,
-  sources = {
-    formatting.prettier.with({
-      prefer_local = "node_modules/.bin"
-    }),
-    formatting.stylua,
-    formatting.rustfmt,
-    formatting.terraform_fmt,
-    -- diagnostics.vale,
-  },
+	debug = false,
+	sources = {
+		formatting.prettier.with({
+			extra_filetypes = { "toml" },
+			prefer_local = "node_modules/.bin",
+		}),
+		formatting.stylua,
+		formatting.rustfmt,
+		formatting.terraform_fmt,
+		diagnostics.flake8,
+		-- diagnostics.vale,
+	},
 })
