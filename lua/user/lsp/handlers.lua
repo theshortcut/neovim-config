412c7088b96f3d92e91ee63597d32455f3ab5cc9
Clayton Ferris <cferris@gmail.com>
2023-05-31 15:18:48 -0700

    update some stale keymaps

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -14,7 +14,7 @@ M.setup = function()
 
     { name = 'DiagnosticSignError', text = '' },
     { name = 'DiagnosticSignWarn', text = '' },
-    { name = 'DiagnosticSignHint', text = '' },
+    { name = 'DiagnosticSignHint', text = '' },
     { name = 'DiagnosticSignInfo', text = '' },
   }
 
@@ -60,9 +60,9 @@ local function lsp_keymaps(bufnr)
   keymap(bufnr, 'n', 'gI', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   keymap(bufnr, 'n', 'gl', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  keymap(bufnr, 'n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting()<cr>', opts)
+  keymap(bufnr, 'n', '<leader>lf', '<cmd>lua vim.lsp.buf.format({ async = true })<cr>', opts)
   keymap(bufnr, 'n', '<leader>li', '<cmd>LspInfo<cr>', opts)
-  keymap(bufnr, 'n', '<leader>lI', '<cmd>LspInstallInfo<cr>', opts)
+  keymap(bufnr, 'n', '<leader>lI', '<cmd>Mason<cr>', opts)
   keymap(bufnr, 'n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
   keymap(bufnr, 'n', '<leader>lj', '<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>', opts)
   keymap(bufnr, 'n', '<leader>lk', '<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>', opts)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,7 +62,6 @@ return packer.startup(function(use)
     'xiyaowong/nvim-transparent',
     config = function()
       require('transparent').setup {
-        enable = true,
         styles = {
           sidebars = "transparent"
         }
