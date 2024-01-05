a8cb0517f7596eed050962053a66ca1682a61b1a
Clayton Ferris <cferris@gmail.com>
2024-01-05 12:58:02 -0700

    update some old configs

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -19,13 +19,9 @@ configs.setup {
     enable = true,
   },
   highlight = {
-    enable = true, -- false will disable the whole extension
+    enable = true,    -- false will disable the whole extension
     disable = { '' }, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
   },
   indent = { enable = true, disable = { 'yaml' } },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
 }
