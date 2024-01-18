ef37863ec73896432190a8dfb6e158294babecb1
Clayton Ferris <cferris@gmail.com>
2024-01-18 16:58:44 -0700

    add crates.nvim

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,5 +1,16 @@
 return {
-  "mrcjkb/rustaceanvim",
-  version = "^3", -- Recommended
-  ft = { "rust" },
+  {
+    "Saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
+    opts = {
+      src = {
+        cmp = { enabled = true },
+      },
+    },
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^3", -- Recommended
+    ft = { "rust" },
+  },
 }
